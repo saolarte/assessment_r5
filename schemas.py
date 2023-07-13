6f0379ad882fab60625a95b1daa8663d9469a0d8
Santiago Olarte <saolarte@msn.com>
2023-07-13 01:53:56 -0500

    add new schemas to support authentication

--- schemas.py
+++ schemas.py
@@ -51,3 +51,22 @@ class LookUpFilters(BaseModel):
 class InsertParameters(BaseModel):
     book_id: str
     source: str
+
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    active: Union[bool, None] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
